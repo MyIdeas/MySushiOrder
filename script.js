d7f778d9118ecf290de5b374d957db6d01d7759c
MyIdeas <marco.versari3@gmail.com>
2023-10-20 15:26:00 +0200

    a

---
+++ script.js
@@ -0,0 +1,5 @@
+// Esempio di gestione del click del pulsante
+document.getElementById('submitButton').addEventListener('click', function () {
+    var inputText = document.getElementById('inputField').value;
+    alert('Hai inserito: ' + inputText);
+});
