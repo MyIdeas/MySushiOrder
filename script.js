9cf5d9542b250544168f625bd6d0f58ed3cc4312
MyIdeas <marco.versari3@gmail.com>
2023-10-20 16:38:42 +0200

    a

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ document.getElementById('submitButton').addEventListener('click', function () {
     } else {
         document.getElementById('errorMessage').textContent = ''; // Cancella il messaggio di errore se il campo è valido
 
-        // Esegui l'azione desiderata quando il campo non è vuoto
-        // Puoi inserire qui il codice per gestire il testo inserito
+        // Cambia schermata mostrando la "seconda schermata"
+        document.querySelector('.second-screen').style.display = 'block';
+
+        // Puoi inserire qui il codice per la seconda schermata o visualizzazione
+   
     }
 
     //alert('Hai inserito: ' + inputText);  non mi serve, è il sito che dice con un pop up ma è brutto
