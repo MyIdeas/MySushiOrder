f5107e4b54da58afda526d350c446e5b1e2ace5c
MyIdeas <marco.versari3@gmail.com>
2023-10-20 15:55:39 +0200

    a

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Esempio di gestione del click del pulsante
 document.getElementById('submitButton').addEventListener('click', function () {
     var inputText = document.getElementById('inputField').value;
-    alert('Hai inserito: ' + inputText);
+
+    if (inputText.trim() === '') {
+        document.getElementById('errorMessage').textContent = 'Il campo non può essere vuoto.';
+    } else {
+        document.getElementById('errorMessage').textContent = ''; // Cancella il messaggio di errore se il campo è valido
+
+        // Esegui l'azione desiderata quando il campo non è vuoto
+        // Puoi inserire qui il codice per gestire il testo inserito
+    }
+
+    //alert('Hai inserito: ' + inputText);  non mi serve, è il sito che dice con un pop up ma è brutto
 });
